8daa4ae056d138dce0f2b62de4c0fd3c43db20da
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-29 13:58:17 -0700

    mvp met, started on stretch

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Scoreboard from "./components/Scoreboard";
 import "./App.css";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-
+  
   return (
     <Scoreboard />
   );
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import BottomRow from "../BottomRow";
 
 export default function Scoreboard() {
+    //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+    const [valueHome, setValueHome] = useState(0);
+    const [valueAway, setValueAway] = useState(0);
+
+    const homeTd = () => {
+        setValueHome(valueHome + 7)
+    }
+
+    const awayTd = () => {
+        setValueAway(valueAway + 7)
+    }
+
+    const homeFg = () => {
+        setValueHome(valueHome + 3)
+    }
+
+    const awayFg = () => {
+        setValueAway(valueAway + 3)
+    }
 
     return (
         <div className="container">
@@ -11,12 +30,12 @@ export default function Scoreboard() {
                     <h2 className="home__name">Lions</h2>
 
                     {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-                    <div className="home__score">32</div>
+                    <div className="home__score">{valueHome}</div>
                 </div>
                 <div className="timer">00:03</div>
                 <div className="away">
                     <h2 className="away__name">Tigers</h2>
-                    <div className="away__score">32</div>
+                    <div className="away__score">{valueAway}</div>
                 </div>
                 </div>
                 <BottomRow />
@@ -25,12 +44,20 @@ export default function Scoreboard() {
                 <div className="homeButtons">
 
                 {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-                <button className="homeButtons__touchdown">Home Touchdown</button>
-                <button className="homeButtons__fieldGoal">Home Field Goal</button>
+                <button
+                onClick = {homeTd}
+                className="homeButtons__touchdown">Home Touchdown</button>
+                <button 
+                onClick = {homeFg}
+                className="homeButtons__fieldGoal">Home Field Goal</button>
                 </div>
                 <div className="awayButtons">
-                <button className="awayButtons__touchdown">Away Touchdown</button>
-                <button className="awayButtons__fieldGoal">Away Field Goal</button>
+                <button 
+                onClick = {awayTd}
+                className="awayButtons__touchdown">Away Touchdown</button>
+                <button 
+                onClick = {awayFg}
+                className="awayButtons__fieldGoal">Away Field Goal</button>
                 </div>
             </section>
         </div>
